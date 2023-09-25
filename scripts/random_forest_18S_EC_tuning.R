63b3f380bc32cf5b38db1cbd85eefe5b62d2cffe
HeatherDeel <heather.deel@usda.gov>
2023-09-25 12:00:32 -0600

    18S tuning script

---
+++ scripts/random_forest_18S_EC_tuning.R
@@ -0,0 +1,8 @@
+#####################################################
+# September 25th, 2023
+# Heather Deel
+# SHAI 18S EC data, tuning only for random forest
+#####################################################
+
+# submitted as bash script in Scinet
+
